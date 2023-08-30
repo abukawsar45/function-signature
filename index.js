41b98a49c24bf0f158e64be95a87de52ddc14a9e
abukawsar45 <kawsar1146236@gmail.com>
2023-08-30 11:21:05 +0600

    signature type

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var myCalculate;
+var myCalculate2;
+var myCalculate3;
 myCalculate = function () {
     console.log('first');
 };
+// singnature
+myCalculate2 = function (name) {
+    console.log("".concat(name, " first"));
+};
+myCalculate3 = function (name, age) {
+    console.log("".concat(name, " and ").concat(age, " years"));
+};
 myCalculate();
+myCalculate2('aks');
+myCalculate3('AbuKawSar', 22);
--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
 let myCalculate: () => void;
+let myCalculate2: (name: string) => void;
+let myCalculate3: (name: string, age: number) => void;
 
 myCalculate = () => {
   console.log('first')
 };
 
+// singnature
+myCalculate2 = (name: string) => {
+  console.log(`${name} first`)
+};
+
+myCalculate3 = (name: string, age: number) => {
+  console.log(`${name} and ${age} years`);
+};
+
 myCalculate();
+myCalculate2('aks');
+myCalculate3('AbuKawSar', 22);
